074fb2537b825faa957c436bdab183815a699903
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-03-08 14:29:11 +0100

    Load spritesheet, render paddles

--- src/pong/mod.rs
+++ src/pong/mod.rs
@@ -6,6 +6,7 @@ use amethyst::ecs::prelude::{Component, DenseVecStorage};
 use amethyst::prelude::*;
 use amethyst::renderer::{
     Camera,
+    Flipped,
     PngFormat,
     Projection,
     SpriteRender,
@@ -30,8 +31,9 @@ impl SimpleState for Pong {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let world = data.world;
         world.register::<Paddle>();
+        let spritesheet_handle = load_spritesheet(world);
 
-        initialize_paddles(world);
+        initialize_paddles(world, spritesheet_handle);
         initialize_camera(world);
     }
 }
@@ -53,7 +55,7 @@ fn initialize_camera(world: &mut World) {
 }
 
 /// Initialize one paddle on the left, and one paddle on the right
-fn initialize_paddles(world: &mut World) {
+fn initialize_paddles(world: &mut World, spritesheet: SpriteSheetHandle) {
     let mut left_transform = Transform::default();
     let mut right_transform = Transform::default();
 
@@ -62,11 +64,18 @@ fn initialize_paddles(world: &mut World) {
     left_transform.set_xyz(PADDLE_WIDTH * 0.5, y, 0.0);
     right_transform.set_xyz(ARENA_WIDTH - PADDLE_WIDTH * 0.5, y, 0.0);
 
+    // Paddle sprites
+    let sprite_render = SpriteRender {
+        sprite_sheet:  spritesheet.clone(),
+        sprite_number: 0, // paddle is the first sprite in the spritesheet
+    };
+
     // Create the left paddle entity
     world
         .create_entity()
         .with(Paddle::new(Side::Left))
         .with(left_transform)
+        .with(sprite_render.clone())
         .build();
 
     // Create the right paddle entity
@@ -74,5 +83,30 @@ fn initialize_paddles(world: &mut World) {
         .create_entity()
         .with(Paddle::new(Side::Right))
         .with(right_transform)
+        .with(sprite_render.clone())
+        .with(Flipped::Horizontal)
         .build();
+}
+
+/// Load the spritesheet for the game
+fn load_spritesheet(world: &mut World) -> SpriteSheetHandle {
+    let loader = world.read_resource::<Loader>();
+    let texture_handle = {
+        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+        loader.load(
+            "texture/pong_spritesheet.png",
+            PngFormat,
+            TextureMetadata::srgb_scale(),
+            (),
+            &texture_storage,
+        )
+    };
+    let spritesheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
+    loader.load(
+        "texture/pong_spritesheet.ron",
+        SpriteSheetFormat,
+        texture_handle,
+        (),
+        &spritesheet_store,
+    )
 }
