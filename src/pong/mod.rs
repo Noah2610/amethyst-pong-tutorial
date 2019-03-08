a56293c79a17817162aae7fb193c450f3be5baaf
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-03-08 14:03:25 +0100

    Initial commit; Basic starter setup with Paddle components

---
+++ src/pong/mod.rs
@@ -0,0 +1,78 @@
+mod components;
+
+use amethyst::assets::{AssetStorage, Loader};
+use amethyst::core::transform::Transform;
+use amethyst::ecs::prelude::{Component, DenseVecStorage};
+use amethyst::prelude::*;
+use amethyst::renderer::{
+    Camera,
+    PngFormat,
+    Projection,
+    SpriteRender,
+    SpriteSheet,
+    SpriteSheetFormat,
+    SpriteSheetHandle,
+    Texture,
+    TextureMetadata,
+};
+
+use components::{Paddle, Side};
+
+pub const ARENA_WIDTH: f32 = 100.0;
+pub const ARENA_HEIGHT: f32 = 100.0;
+
+pub const PADDLE_WIDTH: f32 = 4.0;
+pub const PADDLE_HEIGHT: f32 = 16.0;
+
+pub struct Pong;
+
+impl SimpleState for Pong {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let world = data.world;
+        world.register::<Paddle>();
+
+        initialize_paddles(world);
+        initialize_camera(world);
+    }
+}
+
+/// Initialize a camera entity
+fn initialize_camera(world: &mut World) {
+    let mut transform = Transform::default();
+    transform.set_z(1.0);
+    world
+        .create_entity()
+        .with(Camera::from(Projection::orthographic(
+            0.0,          // Left
+            ARENA_WIDTH,  // Right
+            0.0,          // Bottom (!)
+            ARENA_HEIGHT, // Top    (!)
+        )))
+        .with(transform)
+        .build();
+}
+
+/// Initialize one paddle on the left, and one paddle on the right
+fn initialize_paddles(world: &mut World) {
+    let mut left_transform = Transform::default();
+    let mut right_transform = Transform::default();
+
+    // Correctly position the paddles
+    let y = ARENA_HEIGHT / 2.0;
+    left_transform.set_xyz(PADDLE_WIDTH * 0.5, y, 0.0);
+    right_transform.set_xyz(ARENA_WIDTH - PADDLE_WIDTH * 0.5, y, 0.0);
+
+    // Create the left paddle entity
+    world
+        .create_entity()
+        .with(Paddle::new(Side::Left))
+        .with(left_transform)
+        .build();
+
+    // Create the right paddle entity
+    world
+        .create_entity()
+        .with(Paddle::new(Side::Right))
+        .with(right_transform)
+        .build();
+}
